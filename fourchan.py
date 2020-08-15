661b27903c4231f167ad32ebca5ac4319c50baa7
carp <8250820+capricorn@users.noreply.github.com>
2020-08-14 20:37:17 -0500

    Add 4chan webm scraper to backend

--- fourchan.py
+++ fourchan.py
@@ -1,22 +1,50 @@
 import json
 import shutil
 import re
+from collections import namedtuple
 
 import requests
 from bs4 import BeautifulSoup
+
+#ThreadMetadata = namedtuple('ThreadMetadata', 'id last_modified')
+class ThreadMetadata():
+    def __init__(self, id_, last_modified):
+        self.id = id_
+        self.last_modified = last_modified
+
+    def __hash__(self):
+        return self.id
+
+    def __eq__(self, other):
+        return isinstance(other, ThreadMetadata) and self.id == other.id
+
+    def __lt__(self, other):
+        return isinstance(other, ThreadMetadata) and self.last_modified < other.last_modified
+
+    def __gt__(self, other):
+        return isinstance(other, ThreadMetadata) and self.last_modified > other.last_modified
+
+    def __str__(self):
+        return str(self.id)
+
+    def __repr__(self):
+        return str(self.id)
 
 def get_board_threads(board):
     return requests.get(f'https://a.4cdn.org/{board}/threads.json').json()
 
 def get_active_threads(board):
     threads = get_board_threads(board)
-    return [ thread['no'] for page in threads for thread in page['threads'] ]
+    return [ ThreadMetadata(thread['no'], thread['last_modified']) for page in threads for thread in page['threads'] ]
 
 def get_catalog(board):
     return requests.get(f'https://a.4cdn.org/{board}/catalog.json').json()
 
 def get_thread(board, thread_id):
-    return requests.get(f'https://a.4cdn.org/{board}/thread/{thread_id}.json').json()['posts']
+    ''' Return list of post objects in thread on the given board '''
+    resp = requests.get(f'https://a.4cdn.org/{board}/thread/{thread_id}.json')
+    resp.raise_for_status()
+    return resp.json()['posts']
 
 def download_file(board, url, filename):
     print(f'Downloading file: {filename} from {url}')
---
+++ scraper.py
@@ -0,0 +1,102 @@
+import asyncio
+from collections import namedtuple
+import json
+
+import redis
+from requests.exceptions import HTTPError
+
+import fourchan
+import webm_extractor as extractor
+
+#WebmEntry = namedtuple('WebmEntry', 'id last_modified urls')
+class WebmEntry(fourchan.ThreadMetadata):
+    def __init__(self, thread_metadata, webm_urls):
+        super().__init__(thread_metadata.id, thread_metadata.last_modified)
+        self.urls = webm_urls
+
+async def extract_thread_webms_task(db, queue):
+    print('Extraction worker task started')
+
+    while True:
+        board, thread_metadata = await queue.get()
+        key = f'{board}.{thread_metadata.id}'
+        print(f'Handling extraction for {key}')
+
+        # switch to aiohttp
+        try:
+            thread = fourchan.get_thread(board, thread_metadata.id)
+            webm_urls = [ f'https://i.4cdn.org/{board}/{post["tim"]}.webm' for post in extractor.get_webm_posts(thread) ]
+
+            if 'archived' in thread[0] and thread[0]['archived'] == 1:
+                print('Removing archived thread')
+                db.delete(key)
+            else:
+                # Guess we could just re-set the key.. (can later reload as json)
+                print('Setting keys for thread')
+                db.set(key, json.dumps(WebmEntry(thread_metadata, str(webm_urls)).__dict__))
+        except HTTPError:
+            print('Removing 404\'d thread')
+            db.delete(key)
+        finally:
+            queue.task_done()
+            await asyncio.sleep(1)
+
+# Need to place queue jobs for all current threads for initializing redis
+async def init_catalog(queue, board, catalog):
+    # Start with a blank, fresh db? (flushdb command)
+    # could also print out time in seconds to complete
+
+    print('Initializing webm catalog')
+    print(f'Placing {len(catalog)} extraction tasks')
+    [ queue.put_nowait((board, thread_id)) for thread_id in catalog ]
+    await queue.join()
+
+async def main():
+    db = redis.Redis()
+    # Switch return object to thread?
+    board = 'wsg'
+    # On first run, also need to initialize redis with all of the threads..
+    # Produce a working task list..
+    prev_threads = fourchan.get_active_threads(board)
+    job_queue = asyncio.Queue()
+
+    # Spin up worker task here
+    asyncio.create_task(extract_thread_webms_task(db, job_queue))
+
+    await init_catalog(job_queue, board, prev_threads)
+    print('Finished initializing catalog.')
+
+    while True:
+        await asyncio.sleep(30)
+        print('Checking catalog for changes')
+        #curr_threads = set(fourchan.get_active_threads(board))
+        curr_threads = fourchan.get_active_threads(board)
+
+        # XOR of threads indicates either thread is deleted, or thread is new.
+        # If it's new, key needs added. Old, key needs deleted.. so this could work.
+        catalog_delta = set(curr_threads) ^ set(prev_threads)
+        if catalog_delta != set():
+            print(f'Adding {len(catalog_delta)} thread extraction tasks')
+            [ job_queue.put_nowait((board, thread)) for thread in catalog_delta ]
+
+        # Need to also check threads if their last_modified is different from
+        # those that are on record
+        # (Basically, find all identical pairs where last_modified has changed)
+
+        prev_thread_map = { thread.id: thread for thread in prev_threads }
+
+        updated_threads = [ 
+            thread for thread in curr_threads 
+            if ((thread.id in prev_thread_map) and (thread > prev_thread_map[thread.id]))
+        ]
+
+        if len(updated_threads) > 0:
+            print(updated_threads)
+            print(f'Updating {len(updated_threads)} threads')
+            [ job_queue.put_nowait((board, thread)) for thread in updated_threads ]
+
+        prev_threads = curr_threads
+
+''' Responsible for updating the redis db w/ current threads & webms '''
+if __name__ == '__main__':
+    asyncio.run(main())
